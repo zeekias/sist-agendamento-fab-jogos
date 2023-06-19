d80c548e4bca34f01d1c8487a07397f694637715
Ezequiel Magalhães <ezequiel@csfdigital.corp>
2023-06-19 10:10:48 -0300

    Estrutura das pastas, yarn e tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Login from './components/login/Login';
+function App() {
+  return (
+    <div className="App w-full min-h-screen flex items-center justify-center ">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+const logo = require('../../images/IFMA-LOGO.png');
+
+export default function Login() {
+    return (
+        <div className="min-w-full min-h-screen flex justify-center items-center p-6">
+            <div className="w-[40%] flex justify-center items-center"><img src={logo} alt="" /></div>
+            <main className="w-[50%] flex flex-col justify-center items-center ">
+                <p className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-green-500 md:text-5xl lg:text-6xl dark:text-white">AGGENDER</p>
+                <div className='w-1/2 flex flex-col gap-3'>
+                    <button className="w-full py-4 border rounded-md shadow">Login com o Google</button><br />
+                    <div className='flex flex-col gap-3'>
+                        <input className="w-full p-2 border rounded-md" type="text" placeholder="Email ou nome de usuário" size="25px"></input>
+                        <input className="w-full p-2 border rounded-md" type="password" placeholder="Senha" size="25px"></input>
+                    </div>
+                    <div className="d-flex align-items-center">
+                        <input type="checkbox" name="" id="check" />
+                        <label htmlFor="check" className="ml-1">Manter-se conectado</label>
+                        <a href="" className="ml-3">Esqueceu sua senha?</a>
+                    </div>
+                    <button className="btn cor">Login</button><br />
+                    <a href="">Crie sua conta</a>
+                </div>
+            </main>
+        </div>
+    )
+}
