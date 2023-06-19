ca93493dc039f0e2ed3d03edd3a30ab4e953dc3c
Ezequiel Magalhães <ezequiel@csfdigital.corp>
2023-06-19 13:45:24 -0300

    Notificacoes PopUp

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,20 +1,50 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
-import { FcGoogle } from 'react-icons/fc'
+import { FcGoogle } from 'react-icons/fc';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import Notify from 'simple-notify'
+import 'simple-notify/dist/simple-notify.min.css'
+
 const logo = require('../../images/IFMA-LOGO.png');
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+    const [isToHidePassword, setIsToHidePassword] = useState(true)
     const authContext = useContext(AuthContext);
 
     const handleLoginWithGoogle = () => {
-        authContext.loginByGoogle();
-    }
+        const result = authContext.loginByGoogle();
 
+        if (result.status) {
+            authContext.pushNotify('sucess', result.text, 'Entrando!');
+            return true;
+        }
+
+        authContext.pushNotify('error', result.text, 'Erro ao fazer login com Google!');
+    }
+    const handleHidePassword = () => {
+        setIsToHidePassword(!isToHidePassword);
+    }
     const handleLoginWithEmail = () => {
-        authContext.loginWithEmail(email, password);
+
+        if (!email) {
+            authContext.pushNotify('warning', 'Preencha todos os campos', 'O campo e-mail não pode ficar em branco!');
+            return false;
+        }
+        if (!password) {
+            authContext.pushNotify('warning', 'Preencha todos os campos', 'O campo senha não pode ficar em branco!');
+            return false;
+        }
+
+        const result = authContext.loginWithEmail(email, password);
+
+        if (result.status) {
+            authContext.pushNotify('sucess', result.text, 'Entrando!');
+            return true;
+        }
+
+        authContext.pushNotify('error', result.text, 'Email ou senha inválidos!');
     };
 
     return (
@@ -24,12 +54,20 @@ export default function Login() {
                 <p className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-green-500 md:text-5xl lg:text-6xl dark:text-white">AGGENDER</p>
                 <div className='w-1/2 flex flex-col gap-3'>
                     <button className="w-full py-4 border rounded-md shadow flex items-center justify-center gap-3" onClick={() => handleLoginWithGoogle()}>
-                       <FcGoogle /> <span className='font-bold'> Login com o Google </span>
+                        <FcGoogle /> <span className='font-bold'> Login com o Google </span>
                     </button>
                     <div className='flex flex-col gap-3'>
-                        <input className="w-full p-2 border rounded-md" type="text" placeholder="Email" size="25px" value={email}
-                            onChange={(e) => setEmail(e.target.value)}></input>
-                        <input className="w-full p-2 border rounded-md" type="password" placeholder="Senha" size="25px" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                        <input className="w-full p-2 border rounded-md" type="e-mail" required placeholder="Email" size="25px" value={email}
+                            onChange={(e) => setEmail(e.target.value)} />
+                        <div className='flex justify-between items-center'>
+                            <input className="w-[90%] p-2 border rounded-md" type={`${isToHidePassword && "password"}`} placeholder="Senha" size="25px" value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <div className='w-[8%] flex justify-center items-center'>
+                                {
+                                    isToHidePassword ?
+                                        <AiFillEye size={25} onClick={() => handleHidePassword()} cursor={"pointer"} /> : <AiFillEyeInvisible size={25} onClick={() => handleHidePassword()} cursor={"pointer"} />
+                                }
+                            </div>
+                        </div>
                     </div>
                     <div className="flex">
                         <div className='flex px-2'>
@@ -37,7 +75,7 @@ export default function Login() {
                             <label htmlFor="check" className="ml-1">Manter-se conectado?</label>
                         </div>
                     </div>
-                    <button className="w-full py-4 border rounded-md shadow bg-green-500" onClick={() => handleLoginWithEmail()}> <span className='text-white font-bold'>Login</span></button><br />
+                    <button className="w-full py-4 border rounded-md shadow bg-green-500 hover:bg-green-800" onClick={() => handleLoginWithEmail()}> <span className='text-white font-bold'>Login</span></button><br />
                     <a href="" className="ml-3">Esqueceu sua senha?</a>
                     <a href="">Crie sua conta</a>
                 </div>
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,20 +1,42 @@
 import React, { createContext, useState } from 'react';
 import firebase, { loginWithEmail, loginWithGoogle, logout } from '../services/firebase';
+import Notify from 'simple-notify'
+import 'simple-notify/dist/simple-notify.min.css'
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState('');
 
+    const pushNotify = (statusUp, titleUp, textUp )=>{
+        new Notify({
+          status: statusUp,
+          title: titleUp,
+          text: textUp,
+          effect: 'fade',
+          speed: 300,
+          customClass: null,
+          customIcon: null,
+          showIcon: true,
+          showCloseButton: true,
+          autoclose: true,
+          autotimeout: 3000,
+          gap: 20,
+          distance: 20,
+          type: 1,
+          position: 'right top'
+        })
+      }
+
     const loginByGoogle = async () => {
         const result = await loginWithGoogle();
 
         if (result.status) {
             setUser(result.userToken);
-            return true;
+            return { status: true, text: 'Login bem-sucedido!' };
         }
 
-        return false;
+        return { status: false, text: 'Credenciais inválidas. Tente novamente.' };;
 
     }
 
@@ -22,11 +44,11 @@ export const AuthProvider = ({ children }) => {
         try {
             console.log(email, password);
             const userCredential = await loginWithEmail(email, password);
-            console.log("aaaa", userCredential);
             if (userCredential) {
                 setUser(userCredential.user.accessToken);
                 return { status: true, text: 'Login bem-sucedido!' };
             }
+
             return { status: false, text: 'Credenciais inválidas. Tente novamente.' };
         } catch (error) {
             console.log(error);
@@ -48,7 +70,8 @@ export const AuthProvider = ({ children }) => {
         user,
         loginWithEmail,
         loginByGoogle,
-        logoutContext
+        logoutContext,
+        pushNotify
     };
 
     return (
